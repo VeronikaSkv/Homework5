30683ed40d03d2c9c39a935a0bce597c1f3b861f
Veronika <16nika94@mail.ru>
2023-05-31 23:10:24 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными 
+// числами. Напишите программу, которая покажет количество чётных
+//  чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] GenerateArray ()
+{
+    int [] array = new int [12];
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i]=new Random().Next(100,1000);
+    } return array;
+}
+
+int []array = GenerateArray();
+int Numberchet=0;
+for(int i=0; i<array.Length; i++)
+{
+    if (array[i]%2 == 0)
+    {
+        Numberchet ++;
+    }
+
+}
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine($"Количество четных чисел равно:{Numberchet}");
