9b2af5a46f14ad75e2fae2bff4ad719da9eed9ea
Veronika <16nika94@mail.ru>
2023-06-01 00:13:13 +0300

    Task3

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] Generatearray (int len)
+{
+    int [] array = new int [len];
+    for (int i =0; i<len; i++)
+    {
+        array[i] = new Random().Next(-100,101);
+    }
+return array;
+}
+int [] array =  Generatearray(4);
+int summanumber = 0;
+for (int i=0; i<array.Length; i++)
+{
+    if (i%2 !=0)
+    {
+        summanumber +=array[i];
+    }
+}
+Console.WriteLine(String.Join (",",array));
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях ровна {summanumber}");
---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// Диапазон [-10, 10]. Обратите внимание на вещественных чисел
+// Для примера возьмём диапазон побольше. [3 7 22 2 78] -> 76
+
+int [] GenerateArray (int len)
+{
+    int [] array = new int [len];
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i]=new Random().Next(-100,100);
+    } return array;
+}
+
+int []array = GenerateArray(5);
+int numberMax=array[0];
+int numberMin = array[0];
+for(int i=0; i<array.Length; i++)
+{
+    if (array[i]>numberMax)
+    {
+        numberMax=array[i];
+    }
+else if (array[i]<numberMin)
+{
+   numberMin = array[i];
+}
+}
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine($"Разница между максимальным и минимальным элементом равна:{numberMax-numberMin}");
